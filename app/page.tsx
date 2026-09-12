c990506b8788332f7a2cfac57389fc0a240c13a4
yatendrakumar882-hue <yatendrakumar882@gmail.com>
2026-09-12 08:14:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -67,9 +67,9 @@ export default function BulkEmailSender() {
         remaining: list.length - (sentCount + failedCount),
       });
 
-      // 1 Seconds Delay
+      // 500ms Seconds Delay
       if (i < list.length - 1) {
-        await new Promise((res) => setTimeout(res, 300));
+        await new Promise((res) => setTimeout(res, 200));
       }
     }
 
